4dada7a22386371eb944f0b67269675a8c1903e9
yelson <prl56k>
2019-06-08 00:05:24 -0400

    Agregando Getter y Setter a User

--- src/main/java/edu/utesa/lib/models/dtos/security/UserDto.java
+++ src/main/java/edu/utesa/lib/models/dtos/security/UserDto.java
@@ -13,4 +13,60 @@ public class UserDto {
     private Locale language;
     private Boolean isAdmin = false;
     private Set<PermissionDto> permissionDtos;
+
+    public CompanyDto getCompanyDto() {
+        return companyDto;
+    }
+
+    public void setCompanyDto(CompanyDto companyDto) {
+        this.companyDto = companyDto;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Locale getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Locale language) {
+        this.language = language;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Set<PermissionDto> getPermissionDtos() {
+        return permissionDtos;
+    }
+
+    public void setPermissionDtos(Set<PermissionDto> permissionDtos) {
+        this.permissionDtos = permissionDtos;
+    }
 }

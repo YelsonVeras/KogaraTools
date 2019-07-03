19e90cfd1b91b34ea02d877308266b638e745d86
YelsonVeras <Monasterio.123>
2019-07-02 21:15:17 -0400

    Add DateUtil

--- src/main/java/edu/utesa/lib/utils/DateUtils.java
+++ src/main/java/edu/utesa/lib/utils/DateUtils.java
@@ -6,20 +6,20 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
-class DateUtils {
-    static Date asDate(LocalDate localDate) {
+public class DateUtils {
+    public static Date asDate(LocalDate localDate) {
         return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
     }
 
-    static Date asDate(LocalDateTime localDateTime) {
+    public static Date asDate(LocalDateTime localDateTime) {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
-    static LocalDate asLocalDate(Date date) {
+    public static LocalDate asLocalDate(Date date) {
         return date == null ? null : Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    static LocalDateTime asLocalDateTime(Date date) {
+    public static LocalDateTime asLocalDateTime(Date date) {
         return date == null ? null : Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 }

ea1891bbd17cf5ac6a5df9c8a4e2f67b9857df98
Leidy Laura Martinez Arias <leidylaurama@gmail.com>
2019-08-05 22:50:10 -0400

    Creating ExamDto

---
+++ src/main/java/edu/utesa/lib/models/dtos/school/ExamDto.java
@@ -0,0 +1,38 @@
+package edu.utesa.lib.models.dtos.school;
+
+import edu.utesa.lib.models.dtos.BaseDto;
+import edu.utesa.lib.models.enums.ExamDifficulty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ExamDto extends BaseDto {
+    private TeacherDto teacherDto;
+    private ExamDifficulty examDifficulty;
+    private SubjectDto subjectDto;
+    private TopicDto topicDto;
+    private int problemQuantity;
+    private Date fromDate;
+    private Date toDate;
+    private Set<ProblemDto> problems;
+
+    public String theTeacherName(){
+        return teacherDto.getPersonDto().getFirstNames() + " " + teacherDto.getPersonDto().getLastNames();
+    }
+
+    public String theSubjectName(){
+        return subjectDto.getName();
+    }
+
+    public String theTopicName(){
+        return topicDto.getName();
+    }
+}
---
+++ src/main/java/edu/utesa/lib/models/enums/ExamDifficulty.java
@@ -0,0 +1,5 @@
+package edu.utesa.lib.models.enums;
+
+public enum ExamDifficulty {
+    BASIC, INTERMEDIATE, ADVANCED
+}

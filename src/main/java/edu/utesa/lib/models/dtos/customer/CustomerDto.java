972ce1bd5ca391619d1798b413f65c74da9940f4
yelson <prl56k>
2019-08-07 19:55:34 -0400

    Creando a customer para no editar a client

---
+++ src/main/java/edu/utesa/lib/models/dtos/customer/CustomerDto.java
@@ -0,0 +1,37 @@
+package edu.utesa.lib.models.dtos.customer;
+
+import edu.utesa.lib.models.dtos.BaseDto;
+import edu.utesa.lib.models.dtos.person.PersonDto;
+import edu.utesa.lib.models.dtos.security.UserDto;
+
+import java.math.BigDecimal;
+
+public class CustomerDto extends BaseDto {
+    private PersonDto personDto;
+    private UserDto userDto;
+    private BigDecimal limiter;
+
+    public PersonDto getPersonDto() {
+        return personDto;
+    }
+
+    public void setPersonDto(PersonDto personDto) {
+        this.personDto = personDto;
+    }
+
+    public UserDto getUserDto() {
+        return userDto;
+    }
+
+    public void setUserDto(UserDto userDto) {
+        this.userDto = userDto;
+    }
+
+    public BigDecimal getLimiter() {
+        return limiter;
+    }
+
+    public void setLimiter(BigDecimal limiter) {
+        this.limiter = limiter;
+    }
+}

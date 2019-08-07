3a40d1189e7c755b30efd1ce962ffc217e4ed305
yelson <prl56k>
2019-08-07 19:58:18 -0400

    Terminado de agregar los dto del package customer

--- src/main/java/edu/utesa/lib/models/dtos/customer/ClientDto.java
+++ src/main/java/edu/utesa/lib/models/dtos/customer/ClientDto.java
@@ -2,16 +2,24 @@ package edu.utesa.lib.models.dtos.customer;
 
 import edu.utesa.lib.models.dtos.BaseDto;
 import edu.utesa.lib.models.dtos.company.BranchDto;
-import edu.utesa.lib.models.dtos.company.PositionDto;
 import edu.utesa.lib.models.dtos.person.PersonDto;
 import edu.utesa.lib.models.dtos.security.UserDto;
 
 import java.math.BigDecimal;
 
 public class ClientDto extends BaseDto {
+    private BranchDto branchDto;
     private PersonDto personDto;
     private UserDto userDto;
     private BigDecimal limiter;
+
+    public BranchDto getBranchDto() {
+        return branchDto;
+    }
+
+    public void setBranchDto(BranchDto branchDto) {
+        this.branchDto = branchDto;
+    }
 
     public PersonDto getPersonDto() {
         return personDto;

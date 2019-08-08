ea931c32efbd58da991e9be5a46bdcb0152a154c
Leidy Laura Martinez Arias <leidylaurama@gmail.com>
2019-08-07 22:24:21 -0400

    Adding teacherDto attribute to SubjectDto

--- src/main/java/edu/utesa/lib/models/dtos/security/PermissionDto.java
+++ src/main/java/edu/utesa/lib/models/dtos/security/PermissionDto.java
@@ -1,9 +1,13 @@
 package edu.utesa.lib.models.dtos.security;
 
 import edu.utesa.lib.models.dtos.BaseDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PermissionDto extends BaseDto {
